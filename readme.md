bc5df3eeac0ca33ba5bdf3492d374a59938976bf
Nick Funnell <nick@nick.gs>
2014-10-10 20:51:27 +0000

    init maven setup

---
+++ readme.md
@@ -0,0 +1,4 @@
+# DWBB
+
+A simple application that demonstrates a full Dropwizard server with a Single-page application written in Backbone.
+
---
+++ src/main/java/gs/nick/MyApplication.java
@@ -0,0 +1,12 @@
+package gs.nick;
+
+//import io.dropwizard.Application;
+
+public class MyApplication { // extends Application<MyConfig> {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
+
