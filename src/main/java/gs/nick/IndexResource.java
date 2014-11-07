88927f313d7069710de6558db14d75f7350727f1
Nick F <nick@nick.gs>
2014-11-07 00:08:59 -0800

    Added the /email demo endpoint to convert a pojo to json. Bonus: Netbeans generated a bunch of code for me :)

--- src/main/java/gs/nick/IndexResource.java
+++ src/main/java/gs/nick/IndexResource.java
@@ -1,17 +1,36 @@
 package gs.nick;
 
+import gs.nick.data.EmailEntry;
+import java.util.Date;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/")
 public class IndexResource {
+
+    private Logger oLog;
+
+    public IndexResource() {
+	oLog = LoggerFactory.getLogger(IndexResource.class);
+    }
     
     @GET
     @Produces("text/html")
     public String getPage() {
+	oLog.info("/ resource is being accessed");
         return "Hello, world!";
+    }
+    
+    @GET
+    @Path("/email")
+    @Produces("application/json")
+    public EmailEntry getEmail() {
+	oLog.info("/email resource is being accessed");
+        return new EmailEntry("user@domain.com", "John Smith", new Date(2014, 11, 4));
     }
 
 }
